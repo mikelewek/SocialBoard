86b056f9722b18636869a7426c66751e368ba7de
mikelewek <mikelewek@protonmail.com>
2019-11-16 03:12:49 -0500

    Updates

--- precache-manifest.f2b0cdd4fc4e71c42eb0fd363462f45c.js
+++ precache-manifest.f2b0cdd4fc4e71c42eb0fd363462f45c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/SocialBoard/static/js/runtime~main.a349138b.js"
   },
   {
-    "revision": "1233c07fdcf5b46d4c47",
-    "url": "/SocialBoard/static/js/main.1233c07f.chunk.js"
+    "revision": "5327b8a8bf5e7c96f975",
+    "url": "/SocialBoard/static/js/main.5327b8a8.chunk.js"
   },
   {
     "revision": "ef2b9eb647011e110904",
     "url": "/SocialBoard/static/js/1.ef2b9eb6.chunk.js"
   },
   {
-    "revision": "1233c07fdcf5b46d4c47",
+    "revision": "5327b8a8bf5e7c96f975",
     "url": "/SocialBoard/static/css/main.62f71493.chunk.css"
   },
   {
-    "revision": "76ad6e4d9d6798a853e3899ab8a3ab50",
+    "revision": "e3d55e3c9ceaba199f6beb20e10c7434",
     "url": "/SocialBoard/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/SocialBoard/precache-manifest.b327cac52bc724715ffb831e8882d61d.js"
+  "/SocialBoard/precache-manifest.f2b0cdd4fc4e71c42eb0fd363462f45c.js"
 );
 
 workbox.clientsClaim();
